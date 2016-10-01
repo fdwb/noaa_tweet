f8a7c3ac7b09b5aed45b9f84e79b632c5dda2527
Kaufman, Nick <nicholas.kaufman@noblis.org>
2016-10-01 16:22:07 -0400

    Added functionality to extract tweet information

--- driver.py
+++ driver.py
@@ -6,6 +6,7 @@ import tweepy
 import json
 
 config_fn = "twitter_app_keys.json"
+outfile = "tweets.json"
 
 def main():
     """
@@ -15,9 +16,39 @@ def main():
     auth = authenticate(api_key, api_secret, user_token, user_secret)
 
     api = tweepy.API(auth)
-    api.update_status("...")
+
+    user = api.get_user('NOAA')
+    status_objs = []
+    count = 0
+    for status in limit_handled(tweepy.Cursor(api.user_timeline, id="NOAA").items()):
+        status_obj = process_noaa_tweets(status)
+        status_objs.append(status_obj)
+        count +=1
+
+    print(count)
+    writeout(outfile, status_objs)
 
     return
+
+def process_noaa_tweets(status):
+    """
+    Get relevant information from status object.
+    """
+    return_obj = {}
+    id_str = status.id_str
+    tweet_text = status.text
+    hashtags = []
+    mentioned_usernames = []
+    for obj in status.entities["hashtags"]:
+        hashtags.append(obj["text"])
+    for obj in status.entities["user_mentions"]:
+        mentioned_usernames.append(obj["screen_name"])
+
+    return_obj[id_str] = { "text": tweet_text,
+                            "hashtags": hashtags,
+                            "mentions": mentioned_usernames}
+
+    return return_obj
 
 
 def authenticate(api_key, api_secret, user_token, user_secret):
@@ -49,6 +80,27 @@ def get_keys(fhandle):
     user_secret = key["access_token_secret"]
 
     return api_key, api_secret, user_token, user_secret
+
+def writeout(fhandle, data_arr):
+    """
+    Dump tweets into file
+    """
+    with open(fhandle, "w") as f:
+        for obj in data_arr:
+            f.write(json.dumps(obj, ensure_ascii=False))
+            f.write("\n")
+
+    return
+
+def limit_handled(cursor):
+    """
+    Rate limiting actions, baby!
+    """
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            time.sleep(15 * 60)
 
 
 if __name__ == "__main__":
